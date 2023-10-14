7ef795f8a3209022758b3e989de37e388ea9d507
Mverma124 <147942587+Mverma124@users.noreply.github.com>
2023-10-15 01:30:00 +0530

    Update dbshown.sql

--- E-learning/dbshown.sql
+++ E-learning/dbshown.sql
@@ -1,50 +1,16 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 24, 2023 at 08:09 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `dbshown`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `adminlogin`
---
-
+---
+---this take from xampp
+---
 CREATE TABLE `adminlogin` (
   `id` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `adminlogin`
---
+
 
 INSERT INTO `adminlogin` (`id`, `username`, `password`) VALUES
 (0, 'admin', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comment`
---
 
 CREATE TABLE `comment` (
   `name` varchar(20) NOT NULL,
