4b763f16fdc2da2b57290c9cafd59f54764c38b8
Mverma124 <147942587+Mverma124@users.noreply.github.com>
2023-10-15 01:19:47 +0530

    Update ims_db.sql

--- E-learning/ims_db.sql
+++ E-learning/ims_db.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jul 24, 2023 at 10:05 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `ims_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `course2`
---
 
 CREATE TABLE `course2` (
   `course_id` int(20) NOT NULL,
@@ -37,9 +9,7 @@ CREATE TABLE `course2` (
   `course_original_price` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `course2`
---
+
 
 INSERT INTO `course2` (`course_id`, `course_name`, `course_desc`, `course_author`, `course_duration`, `course_price`, `course_original_price`) VALUES
 (1, 'C programming', 'L;DFJ;LKASD;FLSJAD;LFKJA;LFK', 'Tuhin', '3 month', 123, 5000),
